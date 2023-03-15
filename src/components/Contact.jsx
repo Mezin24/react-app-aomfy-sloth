6e0c4cbc2addd2e2795bdcfa282d7ab34c18c2da
Pavel Mezencev <mezencev24@mail.ru>
2023-03-15 15:28:02 +0300

    add simple pages and formspree sudsribtion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useForm } from '@formspree/react';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  const [state, handleSubmit] = useForm('mzbqvobb');
+  if (state.succeeded) {
+    return (
+      <Wrapper>
+        <div className='section-center'>
+          <h3>Thanks for joining!</h3>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            itaque iste ipsam tenetur voluptate consectetur aperiam, eos totam
+            ex tempore pariatur amet repellat soluta reprehenderit perspiciatis
+            non facilis. Porro, expedita.
+          </p>
+          <form className='contact-form' onSubmit={handleSubmit}>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+              name='email'
+            />
+            <button
+              className='submit-btn'
+              type='submit'
+              disabled={state.submitting}
+            >
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +107,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
