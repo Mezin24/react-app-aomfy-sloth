1897f86114b53c0b4bacd8ca0a21dc3d726e67cb
Pavel Mezencev <mezencev24@mail.ru>
2023-03-15 14:00:34 +0300

    add sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
-import CartButtons from './CartButtons'
-import { useProductsContext } from '../context/products_context'
-import { useUserContext } from '../context/user_context'
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { Link, NavLink } from 'react-router-dom';
+import { links } from '../utils/constants';
+import CartButtons from './CartButtons';
+import { useProductsContext } from '../context/products_context';
+import { useUserContext } from '../context/user_context';
 
 const Nav = () => {
-  return <h4>navbar</h4>
-}
+  const { openSidebar } = useProductsContext();
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='comfy sloth logo' />
+          </Link>
+          <button className='nav-toggle' onClick={openSidebar}>
+            {' '}
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          {links.map(({ id, text, url }) => (
+            <li key={id}>
+              <NavLink to={url}>{text}</NavLink>
+            </li>
+          ))}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  );
+};
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -68,7 +91,8 @@ const NavContainer = styled.nav`
         text-transform: capitalize;
         letter-spacing: var(--spacing);
         padding: 0.5rem;
-        &:hover {
+        &:hover,
+        &.active {
           border-bottom: 2px solid var(--clr-primary-7);
         }
       }
@@ -77,6 +101,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`
+`;
 
-export default Nav
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { ProductsProvider } from './context/products_context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </Router>
   </React.StrictMode>
 );
